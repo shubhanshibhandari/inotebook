268eb0c00d9cd37f461fdb6f042a2ee94d82749f
anick09 <anik3923@gmail.com>
2023-06-07 19:43:26 +0530

    first endpoint created

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -6,9 +6,9 @@ connectToMongo();
 const app = express();
 const port=3000
 
-app.get('/', (req,res) =>{
-    res.send('Hello');
-});
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () =>{
     console.log('listening on port : '+port);
