e5b7a327ff1f2bd3d55707695864a81be8af84ab
anick09 <anik3923@gmail.com>
2023-06-14 22:15:37 +0530

    middleware added

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -4,10 +4,11 @@ const User = require('../models/User');
 const {body, validationResult } = require('express-validator');
 const bcrypt=require('bcryptjs');
 var jwt=require('jsonwebtoken');
+const fetchuser=require('../middleware/fetchuser');
 
 const JWT_SECRET = "Anick@Bhattacharya";
 
-// create a user POST "/api/auth/createuser"
+//ROUTE 1: create a user POST "/api/auth/createuser" (Login not required)
 router.post('/createuser', [
     body('name').isLength({min: 3}),
     body('email').isEmail(),
@@ -52,7 +53,7 @@ router.post('/createuser', [
 });
 
 
-// Login a user POST "/api/auth/login"
+//ROUTE 2: Login a user POST "/api/auth/login" (Login not required)
 router.post('/login', [
     body('email',"Enter valid email").isEmail(),
     body('password',"Password cannot be empty").exists()
@@ -90,5 +91,15 @@ router.post('/login', [
     }
 });
 
-
+//ROUTE 3: Get logged in user details  (Login required)
+router.post('/getuser',fetchuser,async (req,res)=>{
+try {
+    const userId= req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+} catch (err) {
+    console.error(err.message);
+    res.status(500).send("Ineternal Server Error: " + err.message);
+}
+});
 module.exports = router
