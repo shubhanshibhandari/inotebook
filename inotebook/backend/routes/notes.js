a06a3c3ba91a61d1ea4d5c10b3f44c03e20db7be
anick09 <anik3923@gmail.com>
2023-06-15 17:41:51 +0530

    notes update endpoint

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -9,7 +9,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Notes.find({ user: req.user.id });
     res.json(notes);
-  } catch (error) {
+  } catch (err) {
     console.error(err.message);
     res.status(500).json({ "Ineternal Server Error": err.message });
   }
@@ -47,5 +47,31 @@ router.post(
     }
   }
 );
+
+//ROUTE 3: Update a note PUT "api/notes/update/:id" (Login required)
+router.put(
+  "/update/:id",
+  fetchuser,
+  async (req, res) => {
+    const {title,description,tag} = req.body;
+
+    //Create a newNote object
+    const newNote= {};
+    if(title) {newNote.title=title};
+    if(description) {newNote.description=description};
+    if(tag) {newNote.tag=tag};
+
+    //Find the note to be updated and update it
+    let note =await Notes.findById(req.params.id);
+    if(!note) {return res.status(404).send("Note not found");}
+
+    if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true});
+    res.json({note});
+
+  });
 
 module.exports = router;
